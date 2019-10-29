631efa8eaedc0d9f260bdae843ea1e93361a0bd7
AmecomGlo <amecomglobalenterp@gmail.com>
2019-10-29 06:56:04 +0100

    Updates

---
+++ precache-manifest.047a71b45111553cac5e1bc33f3537df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b915bd0addc30b72c8959f3d47701ea4",
+    "url": "%20amecomGlo.github.io/Project_creation/index.html"
+  },
+  {
+    "revision": "5a208eab46efa099b492",
+    "url": "%20amecomGlo.github.io/Project_creation/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3903ec8b333676a4d699",
+    "url": "%20amecomGlo.github.io/Project_creation/static/css/main.c503d416.chunk.css"
+  },
+  {
+    "revision": "5a208eab46efa099b492",
+    "url": "%20amecomGlo.github.io/Project_creation/static/js/2.5c1bc871.chunk.js"
+  },
+  {
+    "revision": "3903ec8b333676a4d699",
+    "url": "%20amecomGlo.github.io/Project_creation/static/js/main.def7c885.chunk.js"
+  },
+  {
+    "revision": "54570438debcb8bc0aa8",
+    "url": "%20amecomGlo.github.io/Project_creation/static/js/runtime-main.faf9ec47.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "%20amecomGlo.github.io/Project_creation/static/media/SEGA.12a3db8b.woff"
+  }
+]);
