cbea3966fc138cce0a93b86f5319bbda6e1651bd
amecomGlo <56518765+amecomGlo@users.noreply.github.com>
2019-10-29 07:06:41 +0100

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,50 @@
+export const robots=[
+  {
+  id:1,
+  Name: "Sanusi Lamido",
+  userName: "Lamido",
+  email: "LamidoSanu@info.com"
+  },
+  {
+  id:2,
+  Name: "Ebere Johnson",
+  userName: "Ebere J",
+  email: "Johnson@info.com"
+  },
+  {
+  id:3,
+  Name: "Lewis Lenus",
+  userName: "lele",
+  email: "Sam@info.com"
+  },
+  {
+  id:4,
+  Name: "Sarah Young",
+  userName: "Young Sara",
+  email: "Learn@info.com"
+  },
+  {
+      id:5,
+      Name: "Sarah Young",
+      userName: "Young Sara",
+      email: "Learn@info.com"   
+  },
+  {
+      id:6,
+  Name: "Sarah Young",
+  userName: "Young Sara",
+  email: "Learn@info.com"
+  },
+  {
+      id:7,
+  Name: "Sarah Young",
+  userName: "Young Sara",
+  email: "Learn@info.com"
+  },
+  {
+      id:8,
+  Name: "Sarah Young",
+  userName: "Young Sara",
+  email: "Learn@info.com"
+  },
+]
